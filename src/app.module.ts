8358cb5b32fb9d57bbb63b4629f13d14cd9299f4
Jonilson Sousa <jonilsonds9@gmail.com>
2024-02-17 17:19:35 -0300

    Add CI/CD e Health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { QueuesModule } from './queues/queues.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    HealthModule,
     QueuesModule,
   ],
   controllers: [AppController],
